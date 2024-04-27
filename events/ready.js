ae667a07ccaaa2616d4e96732243f7f0ae3d492e
EmiWnnn <135044283+EmiWnnn@users.noreply.github.com>
2024-04-27 18:39:19 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+const { ActivityType } = require("discord.js");
+const client = require("../index");
+
+client.on("ready", async () => {
+  console.log(`${client.user.username} Is Online`);
+  client.user.setActivity({
+    name: ("EmiWn ❤️ S T A R", "EmiWn 💛 S T A R", "EmiWn 💚 S T A R", "EmiWn 💜 S T A R", "EmiWn 💙 S T A R", "EmiWn 🤍 S T A R", "EmiWn 🧡 S T A R", "EmiWn 🖤 S T A R", "EmiWn 🤎 S T A R"),
+    type: ActivityType.Listening,
+  });
+
+  // loading database
+  await require("../handlers/Database")(client);
+
+  // loading dashboard
+  require("../server");
+
+  client.guilds.cache.forEach(async (guild) => {
+    await client.updateembed(client, guild);
+  });
+});
